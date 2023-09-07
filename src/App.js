435938917d6818a6d932adc41c0434da1f4295f0
RlackBights <hunor.n.b@gmail.com>
2023-09-07 12:06:37 +0200

    test + install guide

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ function App() {
   return (
     <div className="App">
       <h1>TEST</h1>
-      <p></p>
-
-
-
-
-
-
-
-
-
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, vel
+        numquam placeat doloremque nulla aperiam iure doloribus molestiae dolore
+        cum ex dolorem, nostrum quis quaerat expedita repellat. Aliquid a fuga
+        voluptas laudantium, porro ipsum, odio labore iste sapiente commodi
+        repellendus voluptate accusamus id amet alias dolore facere eum ut quod
+        sequi ab nesciunt? Excepturi vitae numquam, dolorum distinctio
+        voluptatum maiores labore rerum amet. Id, magni labore reprehenderit
+        rerum quisquam quae minima deserunt? Natus sed assumenda culpa repellat
+        reprehenderit sunt, id qui, saepe distinctio velit, ex consectetur! Sit
+        ut provident quidem repellat, perferendis alias, magni voluptatem
+        architecto quod commodi praesentium fugiat?
+      </p>
+      <input type="checkbox" />
     </div>
   );
 }
