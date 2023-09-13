fcde1af336f7f4b32464c40f4118a5d194dd268b
RlackBights <hunor.n.b@gmail.com>
2023-09-13 10:22:19 +0200

    server cleanup

--- server/server.js
+++ server/server.js
@@ -32,7 +32,6 @@ function changeData(req) { updateDB(req.query) }
 
 app.use("/changedata", changeData);
 
-
 /* Error handler middleware */
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -40,49 +39,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).json({ message: err.message });
   return;
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-/*
-const mysql = require("mysql");
-const express = require("express");
-const cors = require("cors");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "LearnTheBasics",
-});
-
-connection.connect(function (err) {
-  if (err) throw err;
-  console.log("Connected!");
-});
-
-function getSaveData() {
-  connection.query("SELECT * FROM savedata", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    return result[0];
-  });
-}
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/message", (req, res) => {
-  res.json({ message: getSaveData() });
-});
-
-app.get("/savefile", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000.`);
-});
-*/
