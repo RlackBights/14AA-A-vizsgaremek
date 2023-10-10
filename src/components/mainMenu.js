e41508d20eb39f6cd01cc706e9239a332e272ad5
RlackBights <hunor.n.b@gmail.com>
2023-10-10 14:41:48 +0200

    update 2

--- src/components/mainMenu.js
+++ src/components/mainMenu.js
@@ -37,11 +37,11 @@ export function MainMenu() {
     
 
     const save1 = useContext(saveContext)[0][0];
-    const setSave1 = useContext(saveContext)[0][2];
+    const setSave1 = useContext(saveContext)[0][1];
     const save2 = useContext(saveContext)[1][0];
-    const setSave2 = useContext(saveContext)[1][2];
+    const setSave2 = useContext(saveContext)[1][1];
     const save3 = useContext(saveContext)[2][0];
-    const setSave3 = useContext(saveContext)[2][2];
+    const setSave3 = useContext(saveContext)[2][1];
 
     async function updateData () {
       let myJson = await getData().then((value) => {
--- src/components/saveCommManager.js
+++ src/components/saveCommManager.js
@@ -1,7 +1,22 @@
 import { cookie } from "./cookie";
 
-export function setData(id, lvl, money, time, cpu, gpu, ram, stg)
+export async function setData(id, lvl, money, time, cpu, gpu, ram, stg)
 {
+
+  let tempSave;
+
+  switch (id) {
+    default:
+      tempSave = await getData()[0];
+      break;
+    case 2:
+      tempSave = await getData()[1];
+      break;
+    case 3:
+      tempSave = await getData()[2];
+      break;
+  }
+
     fetch("http://127.0.0.1:8000/changedata", {
 
         method: "PUT",
@@ -9,13 +24,13 @@ export function setData(id, lvl, money, time, cpu, gpu, ram, stg)
         body: JSON.stringify({
             userAuthCode: cookie.get("user"),
             saveId: id,
-            lvl: lvl,
-            money: money,
-            time: time,
-            cpu: cpu,
-            gpu: gpu,
-            ram: ram,
-            stg: stg,
+            lvl: (lvl == undefined) ? tempSave.lvl : lvl,
+            money: (money == undefined) ? tempSave.money : money,
+            time: (time == undefined) ? tempSave.time : time,
+            cpu: (cpu == undefined) ? tempSave.cpu : cpu,
+            gpu: (gpu == undefined) ? tempSave.gpu : gpu,
+            ram: (ram == undefined) ? tempSave.ram : ram,
+            stg: (stg == undefined) ? tempSave.stg : stg,
         }),
     });
 }
