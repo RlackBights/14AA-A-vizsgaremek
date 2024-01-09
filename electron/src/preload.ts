2afcd789d1067b916efbe0f8187e1e9697181677
RlackBights <114919830+RlackBights@users.noreply.github.com>
2024-01-09 13:05:54 +0100

    Delete electron directory

--- electron/src/preload.ts
+++
@@ -1,9 +0,0 @@
-require('./rt/electron-rt');
-//////////////////////////////
-// User Defined Preload scripts below
-console.log('User Preload!');
-const { ipcRenderer } = require('electron');
-
-document.getElementById('closeApp').addEventListener('click', () => {
-  ipcRenderer.invoke('quit-app');
-});
