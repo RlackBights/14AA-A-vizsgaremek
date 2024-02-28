9220241e5c3ec0d5eeb58db0753f6b642e46e064
RlackBights <hunor.n.b@gmail.com>
2024-02-28 11:03:49 +0100

    added desktop loading screen

--- src/components/desktop.js
+++ src/components/desktop.js
@@ -3,7 +3,20 @@ import React from 'react'
 export function Desktop() {
     return (
         <div id='desktop'>
+            <div class="cube-wrapper" id='loading-screen'>
+                <div class="cube-folding">
+                <span class="leaf1"></span>
+                <span class="leaf2"></span>
+                <span class="leaf3"></span>
+                <span class="leaf4"></span>
+                <span class="leaf5"></span>
+                </div>
+                <span class="loading" data-name="Loading">Learn_OS</span>
+            </div>
+            <div id='taskbar'>
 
+            </div>
+            
         </div>
     )
 }
