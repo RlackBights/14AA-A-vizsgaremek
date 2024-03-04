942adcf1047a35e6c247ef89df7bf34bbe50601b
RlackBights <hunor.n.b@gmail.com>
2024-03-04 14:03:04 +0100

    added cpu to pc view

--- src/components/pcBuild.js
+++ src/components/pcBuild.js
@@ -1,7 +1,9 @@
+import cpu from "../assets/cpu.png";
+
 export function PCBuild() {
     return (
         <div id="pc-build">
-            
+            <img className="draggable-item" src={cpu} alt=""/>
         </div>
     )
 }
